b0af749a57c9f4a6cbdb4b7d22a58a35acc19465
Ananda Guedes do Ó <anandaguedesdoo@gmail.com>
2025-08-25 11:24:42 -0300

    ajustes de arquivos desnecessários

--- BibliOn-Angular_Spring/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: true,
-
-    URL_DESASTRES: 'http://localhost:3000/livros',
-    //URL_VOLUNTEERS: 'http://localhost:3000/volunteers',
-
-}
--- BibliOn-Thymeleaf_Spring/src/main/java/br/edu/ifpb/pweb2/makemerich/model/Natureza.java
+++
@@ -1,5 +0,0 @@
-package br.edu.ifpb.pweb2.makemerich.model;
-
-public enum Natureza {
-    ENTRADA, SAIDA, INVESTIMENTO
-}
